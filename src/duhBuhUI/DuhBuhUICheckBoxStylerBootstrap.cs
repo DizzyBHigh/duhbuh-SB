0857d47daa55a84a54da2fd9853497a0e1fc0985
Dizzy B High <dizzybhigh@gmail.com>
2026-08-17 09:38:17 +0200

    Initialize reusable checkbox styling with the DLL

---
+++ src/duhBuhUI/DuhBuhUICheckBoxStylerBootstrap.cs
@@ -0,0 +1,10 @@
+using System.Runtime.CompilerServices;
+
+public static class DuhBuhUICheckBoxStylerBootstrap
+{
+    [ModuleInitializer]
+    public static void Initialize()
+    {
+        DuhBuhUICheckBoxStyler.Initialize();
+    }
+}
