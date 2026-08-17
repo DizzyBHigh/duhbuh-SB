bd8ea72d4b9b10f8a9e67df10274fd4c3ff193d0
Dizzy B High <dizzybhigh@gmail.com>
2026-08-17 09:44:58 +0200

    Fix checkbox template text binding namespace

--- src/duhBuhUI/DuhBuhUICheckBoxStyler.cs
+++ src/duhBuhUI/DuhBuhUICheckBoxStyler.cs
@@ -3,6 +3,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Media;
 
 public static class DuhBuhUICheckBoxStyler
